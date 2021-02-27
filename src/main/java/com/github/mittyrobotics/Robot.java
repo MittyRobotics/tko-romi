5b08b0160047a669bc9e1a76caa8a833e99b93ce
AndyLi23 <andyliqy@gmail.com>
2021-02-27 13:46:57 -0800

    logic edits, invert talon

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -4,16 +4,29 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 
+
+
 public class Robot extends TimedRobot {
     private Spark leftSpark, rightSpark;
     private DigitalInput aButton, bButton, cButton;
+
+    private int dir;
+    private static volatile boolean wPressed = false;
+
+
     @Override
     public void robotInit() {
         leftSpark = new Spark(Constants.LEFT_MOTOR_ID);
         rightSpark = new Spark(Constants.RIGHT_MOTOR_ID);
+
+        leftSpark.setInverted(false);
+        rightSpark.setInverted(true);
         aButton = new DigitalInput(Constants.A_BUTTON_ID);
         bButton = new DigitalInput(Constants.B_BUTTON_ID);
         cButton = new DigitalInput(Constants.C_BUTTON_ID);
+
+        dir = 0;
+
     }
 
     @Override
@@ -44,16 +57,23 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
-        if(aButton.get()){
+
+        if(dir != 0 && (aButton.get() || bButton.get() || cButton.get())) {
+            leftSpark.set(0);
+            rightSpark.set(0);
+            dir = 0;
+        }
+
+        if(aButton.get() || wPressed){
             leftSpark.set(1);
             rightSpark.set(1);
+            dir = 1;
         } else if(bButton.get()){
             leftSpark.set(-1);
             rightSpark.set(-1);
-        } else if(cButton.get()){
-            leftSpark.set(0);
-            rightSpark.set(0);
+            dir = -1;
         }
+
     }
 
     @Override
