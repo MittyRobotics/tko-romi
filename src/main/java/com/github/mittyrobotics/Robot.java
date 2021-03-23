1460e2a2e594e9aeceeb3026c7b981b4e9a04cfd
WonseoLee24 <wonseolee24@mittymonarch.com>
2021-03-23 16:45:05 -0700

    Gyros & Encoders

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -1,6 +1,7 @@
 package com.github.mittyrobotics;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 
@@ -15,8 +16,67 @@ public class Robot extends TimedRobot {
     /*
      *  INITIALIZE CLASSES HERE
      */
+
+    RomiGyro gyro;
+
+    Spark sparkLeft, sparkRight;
+
+    Encoder encoder = new Encoder(Constants.LEFT_ENCODER_IDS[0], Constants.LEFT_ENCODER_IDS[1]);
+
+    DigitalInput buttonA, buttonB, buttonC;
+
     @Override
     public void robotInit() {
+        gyro = new RomiGyro();
+        gyro.reset();
+
+        encoder.reset();
+
+        encoder.setDistancePerPulse(1./200.);
+
+        encoder.reset();
+
+        sparkLeft = new Spark(Constants.LEFT_MOTOR_ID);
+        sparkRight = new Spark(Constants.RIGHT_MOTOR_ID);
+
+        buttonA = new DigitalInput(Constants.A_BUTTON_ID);
+        buttonB = new DigitalInput(Constants.B_BUTTON_ID);
+        buttonC = new DigitalInput(Constants.C_BUTTON_ID);
+
+        //sparkLeft.setInverted(true);
+        //sparkRight.setInverted(false);
+    }
+
+    //Runs periodically during teleoperated mode
+    /*
+     *  WRITE YOUR DRIVE CODE HERE
+     */
+    @Override
+    public void teleopPeriodic() {
+        double distance = encoder.getDistance();
+        double rate = encoder.getRate();
+
+        encoder.setDistancePerPulse(1./256.);
+        sparkLeft.set(1);
+        sparkRight.set(1);
+        if(distance==5) {
+            sparkLeft.set(0);
+            sparkRight.set(0);
+        }
+
+        if(buttonA.get()) { // left
+            sparkLeft.set(-1);
+            sparkRight.set(1);
+        }
+        else if(buttonB.get()) { // backward
+            sparkLeft.set(-1);
+            sparkRight.set(-1);
+        }
+        else if(buttonC.get()) { // stop
+            sparkLeft.set(0);
+            sparkRight.set(0);
+        }
+
 
     }
 
@@ -47,15 +107,6 @@ public class Robot extends TimedRobot {
     //Runs periodically during autonomous mode
     @Override
     public void autonomousPeriodic() {
-
-    }
-
-    //Runs periodically during teleoperated mode
-    /*
-     *  WRITE YOUR DRIVE CODE HERE
-     */
-    @Override
-    public void teleopPeriodic() {
 
     }
 
