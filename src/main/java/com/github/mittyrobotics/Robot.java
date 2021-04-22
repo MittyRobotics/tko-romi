9555c0609af0b0d52cf6ca1864a88b2308965cf2
DivyanshAgarwal24 <DivyanshAgarwal24@mittymonarch.com>
2021-04-22 16:31:23 -0700

    Day 2 - Encoders and Gyros

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -1,10 +1,8 @@
 package com.github.mittyrobotics;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.controller.PIDController;
 
 
 //Java automatically runs this class, and calls the various functions.
@@ -17,16 +15,43 @@ public class Robot extends TimedRobot {
     /*
      *  INITIALIZE CLASSES HERE
      */
-    Spark SparkLeft, SparkRight;
+    /* Day 1
+    Spark sparkLeft, sparkRight;
     DigitalInput left_button, right_button, middle_button;
+    */
+
+    /* Day 2 leftEncoder = example variable | encoder and spark = challenge variables */
+    Encoder leftEncoder;
+    Spark spark;
+    Encoder encoder;
+
+    RomiGyro gyro;
+    Spark leftSpark, rightSpark;
 
     @Override
     public void robotInit() {
-        SparkLeft = new Spark(0);
-        SparkRight = new Spark(1);
+        /* Day 1
+        sparkLeft = new Spark(0);
+        sparkRight = new Spark(1);
         left_button = new DigitalInput(0);
         right_button = new DigitalInput(1);
         middle_button = new DigitalInput(2);
+         */
+
+        leftEncoder = new Encoder(Constants.LEFT_ENCODER_IDS[0], Constants.LEFT_ENCODER_IDS[1]);
+        leftEncoder.reset();
+        leftEncoder.setDistancePerPulse(1./10.);
+        leftEncoder.setMinRate(10.);
+        leftEncoder.setReverseDirection(true);
+        spark = new Spark(Constants.SPARK_ID);
+        encoder = new Encoder(Constants.ENCODER_IDS[0], Constants.ENCODER_IDS[1]);
+        encoder.reset();
+        encoder.setDistancePerPulse(1./256.);
+
+        gyro = new RomiGyro();
+        gyro.reset();
+        leftSpark = new Spark(Constants.LEFT_MOTOR_ID);
+        rightSpark = new Spark(Constants.RIGHT_MOTOR_ID);
     }
 
     //Runs periodically during teleoperated mode
@@ -35,29 +60,46 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void teleopPeriodic() {
+        /* Day 1
         if (left_button.get() && right_button.get() && middle_button.get()) {
-            SparkLeft.set(-0.5);
-            SparkRight.set(-0.5);
+            sparkLeft.set(-0.5);
+            sparkRight.set(-0.5);
+        } else if (middle_button.get() && left_button.get()) {
+            sparkLeft.set(-0.5);
+        } else if (middle_button.get() && left_button.get()) {
+            sparkRight.set(-0.5);
+        } else if (left_button.get() && right_button.get()) {
+            sparkLeft.set(0.5);
+            sparkRight.set(0.5);
+        } else if (left_button.get()) {
+            sparkLeft.set(0.5);
+        } else if (right_button.get()) {
+            sparkRight.set(0.5);
+        } else if (!left_button.get() && !right_button.get() && !middle_button.get()) {
+            sparkLeft.set(0);
+            sparkRight.set(0);
         }
-        else if (middle_button.get() && left_button.get()) {
-            SparkLeft.set(-0.5);
+        */
+
+        double distance = leftEncoder.getDistance();
+        double rate = leftEncoder.getRate();
+        if (encoder.getDistance() >= 5) {
+            spark.set(0);
+        } else {
+            spark.set(0.5);
         }
-        else if (middle_button.get() && left_button.get()) {
-            SparkRight.set(-0.5);
-        }
-        else if (left_button.get() && right_button.get()) {
-            SparkLeft.set(0.5);
-            SparkRight.set(0.5);
-        }
-        else if (left_button.get()) {
-            SparkLeft.set(0.5);
-        }
-        else if (right_button.get()) {
-            SparkRight.set(0.5);
-        }
-        else if (!left_button.get() && !right_button.get() && !middle_button.get()) {
-            SparkLeft.set(0);
-            SparkRight.set(0);
+
+
+        double angleZ = gyro.getAngleZ();
+        if (angleZ < 90) {
+            leftSpark.set(0.5);
+            rightSpark.set(-0.5);
+        } else if (angleZ > 90) {
+            rightSpark.set(0.5);
+            leftSpark.set(-0.5);
+        } else {
+            leftSpark.set(0);
+            rightSpark.set(0);
         }
     }
 
