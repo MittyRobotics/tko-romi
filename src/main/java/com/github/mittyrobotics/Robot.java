57e9736392aa8893d8ce2c78ed12c8ea8e8c3944
Kazu-Pang <72171546+Kazu-Pang@users.noreply.github.com>
2021-04-27 16:45:52 -0700

    PID controllers day 4

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -18,17 +18,28 @@ public class Robot extends TimedRobot {
      *  INITIALIZE CLASSES HERE
      */
 
-    Encoder leftEncoder;
-    Encoder encoder;
-Spark Spark;
-
-RomiGyro gyro;
-Spark leftSpark, rightSpark;
+//    Encoder leftEncoder;
+//    Encoder encoder;
+//Spark Spark;
+//
+//RomiGyro gyro;
+//Spark leftSpark, rightSpark;
 //    Spark SparkLeft, SparkRight;
 //    DigitalInput LeftButton, RightButton;
 
+PIDController controller;
+Spark leftSpark, rightSpark;
+Encoder encoder;
+
     @Override
     public void robotInit() {
+        controller = new PIDController(0.5, 0.001, 0.2); //3 doubles
+        leftSpark = new Spark(Constants.LEFT_MOTOR_ID);
+        rightSpark = new Spark(Constants.RIGHT_MOTOR_ID);
+        encoder = new Encoder(Constants.LEFT_ENCODER_IDS[0], Constants.LEFT_ENCODER_IDS[1]);
+        encoder.setDistancePerPulse (1./50.);
+        encoder.reset();
+
 //SparkLeft = new Spark(0);
 //SparkRight = new Spark(1);
 //DigitalInput LeftButton = new DigitalInput();
@@ -40,15 +51,15 @@ Spark leftSpark, rightSpark;
 //        leftEncoder.setMinRate(10.); //10 inches / sec
 //        leftEncoder.setReverseDirection(true);
 
-        Spark = new Spark(Constants.SPARK_ID);
-        encoder = new Encoder(Constants.ENCODER_IDS[0], Constants.ENCODER_IDS[1]);
-        encoder.reset()
-        encoder.setDistancePerPulse(1./256.);
-
-        gyro = new RomiGyro();
-        gyro.reset();
-        leftSpark = new Spark(Constants.LEFT_MOTOR_ID);
-        rightSpark = new Spark(Constants.RIGHT_MOTOR_ID);
+//        Spark = new Spark(Constants.SPARK_ID);
+//        encoder = new Encoder(Constants.ENCODER_IDS[0], Constants.ENCODER_IDS[1]);
+//        encoder.reset()
+//        encoder.setDistancePerPulse(1./256.);
+//
+//        gyro = new RomiGyro();
+//        gyro.reset();
+//        leftSpark = new Spark(Constants.LEFT_MOTOR_ID);
+//        rightSpark = new Spark(Constants.RIGHT_MOTOR_ID);
     }
 
     @Override
@@ -60,20 +71,20 @@ Spark leftSpark, rightSpark;
 //        double rate = leftEncoder.getRate();
 //double xAngle = gyro.getAngleX(); To get angle of the gyro on an axis
 
-        if (encoder.getDistance() < 5) {
-            Spark.set(1);
-        } else {
-            Spark.set(0);
-        }
-
-        double zAngle = gyro.getAngleZ();
-        if (zAngle < 90) {
-            leftSpark.set(-1);
-            rightSpark.set(1);
-    } else {
-            leftSpark.set(0);
-            rightSpark.set(0);
-        }
+//        if (encoder.getDistance() < 5) {
+//            Spark.set(1);
+//        } else {
+//            Spark.set(0);
+//        }
+//
+//        double zAngle = gyro.getAngleZ();
+//        if (zAngle < 90) {
+//            leftSpark.set(-1);
+//            rightSpark.set(1);
+//    } else {
+//            leftSpark.set(0);
+//            rightSpark.set(0);
+//        }
 
 //When both buttons are not pressed, the robot will move backwards
 //        if (!DigitalInput.get(LeftButton) && !DigitalInput.get(RightButton)) {
@@ -90,6 +101,13 @@ Spark leftSpark, rightSpark;
 //            SparkLeft.set(-1);
 //            SparkRight.set(1);
 //}
+        if (Math.abs(controller.getPositionError()) > 0.2) {
+            leftSpark.set(controller.calculate(encoder.getDistance())); // encoder value first, setpoint
+            rightSpark.set(controller.calculate(encoder.getDistance()));
+        } else {
+            leftSpark.set(0);
+            rightSpark.set(0);
+        }
     }
 
 
@@ -102,7 +120,7 @@ Spark leftSpark, rightSpark;
     //Runs when teleoperated mode (robot controlled by driver) is first activated
     @Override
     public void teleopInit() {
-
+controller.setSetpoint(10.);
     }
 
     //Runs when test mode is activated
