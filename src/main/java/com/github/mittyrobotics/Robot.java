380203d2e262102df4e1d227265bfa95a3d7d1c8
DivyanshAgarwal24 <DivyanshAgarwal24@mittymonarch.com>
2021-05-01 17:12:53 -0700

    Romi Testing #1

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.controller.PIDController;
 
+import java.util.ArrayList;
+
 
 //Java automatically runs this class, and calls the various functions.
 /*
@@ -17,38 +19,51 @@ public class Robot extends TimedRobot {
      *  INITIALIZE CLASSES HERE
      */
 
-    /* Day 1
+    /** Day 1
     Spark sparkLeft, sparkRight;
     DigitalInput left_button, right_button, middle_button;
-    */
+    **/
 
-    /* Day 2 leftEncoder = example variable | encoder and spark = challenge variables
+    /** Day 2 leftEncoder = example variable | encoder and spark = challenge variables
     Encoder leftEncoder;
     Spark spark;
     Encoder encoder;
 
     RomiGyro gyro;
     Spark leftSpark, rightSpark;
-     */
+     **/
 
-    /* Day 3
+    /** Day 3
     PIDController controller;
     Spark leftSpark, rightSpark;
     Encoder encoder;
-    */
+    **/
+
+    Spark leftSpark, rightSpark;
+
+    Encoder leftEncoder, rightEncoder;
+
+    RomiGyro gyro;
+    double angleX;
+    double angleY;
+    double angleZ;
+
+    PIDController leftController, rightController;
+
+    int step;
 
 
     @Override
     public void robotInit() {
-        /* Day 1
+        /** Day 1
         sparkLeft = new Spark(0);
         sparkRight = new Spark(1);
         left_button = new DigitalInput(0);
         right_button = new DigitalInput(1);
         middle_button = new DigitalInput(2);
-         */
+         **/
 
-        /* Day 2
+        /** Day 2
         leftEncoder = new Encoder(Constants.LEFT_ENCODER_IDS[0], Constants.LEFT_ENCODER_IDS[1]);
         leftEncoder.reset();
         leftEncoder.setDistancePerPulse(1./10.);
@@ -63,16 +78,35 @@ public class Robot extends TimedRobot {
         gyro.reset();
         leftSpark = new Spark(Constants.LEFT_MOTOR_ID);
         rightSpark = new Spark(Constants.RIGHT_MOTOR_ID);
-         */
+        **/
 
-        /* Day 3
+        /** Day 3
         controller = new PIDController(0.5, 0.0001, 0.2);
         leftSpark = new Spark(Constants.LEFT_MOTOR_ID);
         rightSpark = new Spark(Constants.RIGHT_MOTOR_ID);
         encoder = new Encoder(Constants.ENCODER_IDS[0], Constants.ENCODER_IDS[1]);
         encoder.setDistancePerPulse(1./Constants.TICKS_PER_INCH);
         encoder.reset();
-         */
+        **/
+
+        leftSpark = new Spark(Constants.LEFT_MOTOR_ID);
+        rightSpark = new Spark(Constants.RIGHT_MOTOR_ID);
+        rightSpark.setInverted(true);
+
+        leftEncoder = new Encoder(Constants.LEFT_ENCODER_IDS[0], Constants.LEFT_ENCODER_IDS[1]);
+        leftEncoder.setDistancePerPulse(1./Constants.TICKS_PER_INCH);
+        rightEncoder = new Encoder(Constants.RIGHT_ENCODER_IDS[0], Constants.RIGHT_ENCODER_IDS[1]);
+        rightEncoder.setDistancePerPulse(1./Constants.TICKS_PER_INCH);
+        leftEncoder.reset();
+        rightEncoder.reset();
+
+        gyro = new RomiGyro();
+        gyro.reset();
+
+        leftController = new PIDController(7, 0.0000000000001, 0.2);
+        rightController = new PIDController(7, 0.0000000000001, 0.2);
+
+        step = 0;
     }
     
     //Runs periodically during teleoperated mode
@@ -81,7 +115,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void teleopPeriodic() {
-        /* Day 1
+        /** Day 1
         if (left_button.get() && right_button.get() && middle_button.get()) {
             sparkLeft.set(-0.5);
             sparkRight.set(-0.5);
@@ -100,9 +134,9 @@ public class Robot extends TimedRobot {
             sparkLeft.set(0);
             sparkRight.set(0);
         }
-        */
+        **/
 
-        /* Day 2
+        /** Day 2
         double distance = leftEncoder.getDistance();
         double rate = leftEncoder.getRate();
         if (encoder.getDistance() >= 5) {
@@ -123,9 +157,9 @@ public class Robot extends TimedRobot {
             leftSpark.set(0);
             rightSpark.set(0);
         }
-         */
+         **/
 
-        /* Day 3
+        /** Day 3
         if (Math.abs(controller.getPositionError()) > 0.2) {
             leftSpark.set(controller.calculate(encoder.getDistance()));
             rightSpark.set(controller.calculate(encoder.getDistance()));
@@ -133,7 +167,53 @@ public class Robot extends TimedRobot {
             leftSpark.set(0);
             rightSpark.set(0);
         }
-        */
+        **/
+
+        if (step == 0) {
+            if (leftEncoder.getDistance() < 10) {
+                leftSpark.set(0.125);
+            } else if (leftEncoder.getDistance() < 15) {
+                leftSpark.set(0.1);
+            } else if (leftEncoder.getDistance() < 20) {
+                leftSpark.set(0.1);
+            } else {
+                leftSpark.set(0);
+            }
+
+            if (rightEncoder.getDistance() < 10) {
+                rightSpark.set(0.125);
+            } else if (rightEncoder.getDistance() < 15) {
+                rightSpark.set(0.1);
+            } else if (rightEncoder.getDistance() < 20) {
+                rightSpark.set(0.1);
+            } else {
+                rightSpark.set(0);
+            }
+
+            if (leftSpark.get() == 0 && rightSpark.get() == 0) {
+                step++;
+            }
+        }
+        if (step == 1) {
+            angleX = gyro.getAngleX();
+            angleY = gyro.getAngleY();
+            angleZ = gyro.getAngleZ();
+
+            if (angleZ > -90) {
+                leftSpark.set(-0.1);
+                rightSpark.set(0.1);
+            } else if (angleZ < -90) {
+                leftSpark.set(0.1);
+                rightSpark.set(-0.1);
+            } else {
+                leftSpark.set(0);
+                rightSpark.set(0);
+            }
+        }
+
+        // leftSpark.set(leftController.calculate(leftEncoder.getDistance()));
+        // rightSpark.set(rightController.calculate(rightEncoder.getDistance()));
+
     }
 
     //Runs when antonomous mode (robot runs on its own) first activated via the desktop application
@@ -145,8 +225,12 @@ public class Robot extends TimedRobot {
     //Runs when teleoperated mode (robot controlled by driver) is first activated
     @Override
     public void teleopInit() {
-        // Day 3
-        // controller.setSetpoint(10.);
+        /** Day 3
+         controller.setSetpoint(10.);
+         **/
+
+        leftController.setSetpoint(10);
+        rightController.setSetpoint(10);
     }
 
     //Runs when test mode is activated
