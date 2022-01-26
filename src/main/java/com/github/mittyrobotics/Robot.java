701750655a36f0eccd9554bfc5c8135b24c72a76
NitinVadulas25 <91292971+NitinVadulas25@users.noreply.github.com>
2022-01-25 17:22:35 -0800

    Day 2 Code- Drive Code- Nitin

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -1,37 +1,119 @@
 package com.github.mittyrobotics;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.Joystick.*;
 import edu.wpi.first.wpilibj.controller.PIDController;
 
 
-//Java automatically runs this class, and calls the various functions.
-/*
- *  YOUR WIFI MUST BE CONNECTED TO ROMI FOR THIS TO WORK
- */
 public class Robot extends TimedRobot {
 
-    //Runs when the robot is first started up and should be used for any initialization code
-    /*
-     *  INITIALIZE CLASSES HERE
-     */
+    DigitalInput digitalInput4;
+    DigitalInput digitalInput1;
+    DigitalInput digitalInput2;
+    DigitalInput digitalInput3;
 
 
+
+
+
+    Spark SparkLeft, SparkRight;
 
     @Override
     public void robotInit() {
 
+        SparkLeft = new Spark(0);
+        SparkRight = new Spark(1);
 
-    }
+        SparkLeft.setInverted(true);
 
-    //Runs periodically during teleoperated mode
-    /*
-     *  WRITE YOUR DRIVE CODE HERE
-     */
+
+
+
+        digitalInput4 = new DigitalInput(4);
+
+        if (digitalInput4.get())
+            SparkLeft.set(1);
+            SparkRight.set(-1);
+
+
+
+
+        digitalInput1 = new DigitalInput(1);
+
+        if (digitalInput1.get())
+            SparkLeft.set(-1);
+            SparkRight.set(1);
+
+
+
+
+
+        //forward
+        digitalInput2 = new DigitalInput(2);
+
+
+
+
+
+
+        //back
+        digitalInput3 = new DigitalInput(3);
+
+        if (digitalInput3.get())
+            SparkLeft.set(-1);
+            SparkRight.set(-1);
+
+
+
+
+
+
+
+        }
+
+
+
+    RomiGyro gyro;
+
+
+    XboxController controller = new XboxController(0);
+
+
     @Override
     public void teleopPeriodic() {
+        if (digitalInput4.get())
+            SparkLeft.set(1);
+            SparkRight.set(-1);
+
+        if (digitalInput1.get())
+            SparkLeft.set(-1);
+            SparkRight.set(1);
+
+        if (digitalInput2.get())
+            SparkLeft.set(1);
+            SparkRight.set(1);
+
+
+
+
+
+        if (controller.getAButton())
+
+
+
+            while (gyro.getAngleZ() < 45) {
+                SparkLeft.set(1);
+                SparkRight.set(-1);
+
+            }
+
+            if (controller.getAButton() && gyro.getAngleZ() < 45) {
+                SparkLeft.set(1);
+                SparkRight.set(-1);
+            }
+
+
+
 
     }
 
@@ -41,7 +123,6 @@ public class Robot extends TimedRobot {
 
     }
 
-    //Runs when teleoperated mode (robot controlled by driver) is first activated
     @Override
     public void teleopInit() {
 
