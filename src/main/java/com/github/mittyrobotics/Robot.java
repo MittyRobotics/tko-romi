056ec8b8ba9bfe1c9101a0ff5eaa4882fbd98f42
rohankoduru <rohankoduru25@mittymonarch.com>
2022-02-01 17:08:25 -0800

    TankDrive

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -17,19 +17,66 @@ public class Robot extends TimedRobot {
     /*
      *  INITIALIZE CLASSES HERE
      */
+    Spark SparkLeft, SparkRight;
+    XboxController controller = new XboxController(0);
+
 
 
 
     @Override
     public void robotInit() {
+        SparkLeft = new Spark(0); // 0 is example of left channel
+        SparkRight = new Spark(1); // 1 is example of right channel
+        //SparkLeft.setInverted(true); // boolean parameter example
+        //SparkRight.setInverted(false); // boolean parameter example
+
+
+
 
 
     }
+
+
 
     //Runs periodically during teleoperated mode
     /*
      *  WRITE YOUR DRIVE CODE HERE
      */
+    public void challenge() {
+        double y = controller.getY(GenericHID.Hand.kLeft);
+        double y2 = controller.getY(GenericHID.Hand.kRight);
+        double x = controller.getX(GenericHID.Hand.kLeft);
+        double x2 = controller.getX(GenericHID.Hand.kRight);
+        //forward command
+        if (y > 0 && y <= 1) {
+            if (y2 > 0 && y2 <= 1) {
+                // go forward spark commands
+                SparkLeft.set(1);
+                SparkRight.set(1);
+            }
+        }
+        //go backwards
+        else if (y < 0 && y >= -1) {
+            if (y2 < 0 && y2 >= -1){
+                // go backward spark commands
+                SparkLeft.set(-1);
+                SparkRight.set(-1);
+            }
+        }
+        //go right
+        else if (x2 > 0 && x2 <= 1) {
+            //go right spark command
+            SparkRight.set(1);
+            SparkLeft.set(0);
+        }
+        // go left
+        else if (x < 0 && x >= -1) {
+            //go left spark command
+            SparkRight.set(1);
+            SparkRight.set(0);
+        }
+    }
+
     @Override
     public void teleopPeriodic() {
 
@@ -38,19 +85,24 @@ public class Robot extends TimedRobot {
     //Runs when antonomous mode (robot runs on its own) first activated via the desktop application
     @Override
     public void autonomousInit() {
+        SparkLeft.set(1);
+        SparkRight.set(-1);
+
 
     }
 
     //Runs when teleoperated mode (robot controlled by driver) is first activated
     @Override
     public void teleopInit() {
-
+        SparkLeft.set(-1);
+        SparkRight.set(1);
     }
 
     //Runs when test mode is activated
     @Override
     public void testInit() {
-
+        SparkLeft.set(-1);
+        SparkRight.set(-1);
     }
 
     //Runs whenever the robot is on, periodically: should be used for command schedulers
