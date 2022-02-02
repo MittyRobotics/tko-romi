42d7a111d24713a389a0b57efeb528df8d28228f
NoraLal25 <noralal25@mittymonarch.com>
2022-02-01 17:07:42 -0800

    Tank Drive

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -19,7 +19,10 @@ public class Robot extends TimedRobot {
     public static DigitalInput digitalInputA, digitalInputB, digitalInputC, digitalInputD;
     public static DigitalOutput digitalOutputRed;
 
-    XboxController controller;
+    public static XboxController leftController;
+    public static XboxController rightController;
+    public static XboxController controller;
+    public static boolean isActivated = false;
     RomiGyro gyro;
 
     @Override
@@ -34,9 +37,11 @@ public class Robot extends TimedRobot {
         digitalInputB = new DigitalInput(Constants.B_BUTTON_ID);
         digitalInputC = new DigitalInput(Constants.C_BUTTON_ID);
         digitalInputD = new DigitalInput(Constants.D_BUTTON_ID);
-        digitalOutputRed = new DigitalOutput(Constants.RED_LED_ID);
+        //digitalOutputRed = new DigitalOutput(Constants.RED_LED_ID);
 
-        controller = new XboxController(0);
+        leftController = new XboxController(0);
+        rightController = new XboxController(1);
+        controller = new XboxController(2);
         gyro = new RomiGyro();
     }
 
@@ -47,19 +52,41 @@ public class Robot extends TimedRobot {
     @Override
     public void teleopPeriodic() {
 
+/*
         if (digitalInputA.get()) {
             SparkLeft.set(-1);
             SparkRight.set(1);
-        }else if (digitalInputB.get()) {
+        } else if (digitalInputB.get()) {
             SparkLeft.set(1);
             SparkRight.set(-1);
             //digitalOutputRed.set(true);
-        }else if (digitalInputC.get()) {
+        } else if (digitalInputC.get()) {
             SparkLeft.set(1);
             SparkRight.set(1);
-        }else if (digitalInputD.get()) {
+        } else if (digitalInputD.get()) {
             SparkLeft.set(-1);
             SparkRight.set(-1);
+        }
+ */
+        if (controller.getAButtonPressed()) {
+            isActivated = true;
+        } else if (controller.getBButtonPressed()) {
+            isActivated = false;
+        }
+        if (isActivated) {
+            if (leftController.getAButtonPressed()) {
+                SparkLeft.set(0.25);
+            } else if (leftController.getBButtonPressed()) {
+                SparkLeft.set(-0.25);
+            } else if (leftController.getAButtonReleased() || leftController.getBButtonReleased() ) {
+                SparkLeft.set(0);
+            } else if (rightController.getAButtonPressed()) {
+                SparkRight.set(0.25);
+            } else if (rightController.getBButtonPressed()) {
+                SparkRight.set(-0.25);
+            } else if (rightController.getAButtonReleased() || rightController.getBButtonReleased() ) {
+                SparkRight.set(0);
+            }
         } else {
             SparkLeft.set(0);
             SparkRight.set(0);
@@ -67,15 +94,17 @@ public class Robot extends TimedRobot {
         /*
         if (controller.getAButton()) {
             while (gyro.getAngleZ() < 45) {
-                sparkLeft.set(0.5);
-                sparkRight.set(0.5);
+                SparkLeft.set(0.5);
+                SparkRight.set(0.5);
             }
         }
         */
+        /*
         if (controller.getAButton() && gyro.getAngleZ() <  45) {
-            sparkRight.set(0.5);
-            sparkLeft.set(-0.5);
+            SparkRight.set(0.5);
+            SparkLeft.set(-0.5);
         }
+         */
     }
 
     //Runs when antonomous mode (robot runs on its own) first activated via the desktop application
