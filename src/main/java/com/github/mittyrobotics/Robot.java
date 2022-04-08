21badcc60b903b0e425d699478eed82a98df5409
DhruvBharadwaj25 <dhruvbharadwaj25@mittymonarch.com>
2022-04-08 16:58:03 -0700

    updated romi challenge

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -1,17 +1,12 @@
 package com.github.mittyrobotics;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-
 import static com.github.mittyrobotics.Constants.*;
-import static java.awt.Color.*;
 
 
 //Java automatically runs this class, and calls the various functions.
@@ -34,12 +29,16 @@ public class Robot extends TimedRobot {
     int counter1, counter2, counter3, counter4, counter5, counter6, counter7, counter8;
     Encoder encoder;
     PIDController controller;
-    AddressableLED greenLight;
-    AddressableLED redLight;
-    AddressableLED yellowLight;
+//  AddressableLED greenLight;
+//  AddressableLED redLight;
+//  AddressableLED yellowLight;
+    double oldAngle;
+    int counting;
+    double distanceTraveled;
 
     @Override
     public void robotInit() {
+        System.out.println("Robot init working");
         sparkRight = new Spark(Constants.RIGHT_MOTOR_ID);
         sparkLeft = new Spark(Constants.LEFT_MOTOR_ID);
         gyro = new RomiGyro();
@@ -59,138 +58,70 @@ public class Robot extends TimedRobot {
         counter8 = 0;
         double kp = 0.0, ki = 0.0, kd = 0.0;
         controller = new PIDController(kp, ki, kd);
-        greenLight = new AddressableLED(1);
-        redLight = new AddressableLED(2);
-        yellowLight = new AddressableLED(3);
+//      greenLight = new AddressableLED(1);
+//      redLight = new AddressableLED(2);
+//      yellowLight = new AddressableLED(3);
+        oldAngle = 0;
+        counting = 1;
+        distanceTraveled = 0;
     }
+
+    public void moveForward(int inchesToTravel, int stepNumber) {
+        if (counting == stepNumber) {
+            if (distanceTraveled < encoder.getDistance() - inchesToTravel) {
+                TrapezoidProfile.State profileOutput = profile.calculate(0.02 * counter1);
+                controller.setSetpoint(12 * TICKS_PER_INCH);
+                double output = controller.calculate(encoder.getDistance());
+                sparkRight.set(output);
+                sparkLeft.set(output);
+                counter1++;
+            }
+            counting ++;
+            distanceTraveled += inchesToTravel;
+        }
+    }
+
+    public void turn (double angleToTurn, int stepNumber, boolean rightTurn) {
+        if (stepNumber == counting) {
+            if (rightTurn) {
+                if (gyro.getAngleZ() < oldAngle + angleToTurn) {
+                    sparkLeft.set(0.5);
+                    sparkRight.set(-0.5);
+                }
+                oldAngle = oldAngle + angleToTurn;
+            }
+            else {
+                if (gyro.getAngleZ() > oldAngle - angleToTurn) {
+                    sparkLeft.set(-0.5);
+                    sparkRight.set(0.5);
+                }
+                oldAngle = oldAngle - angleToTurn;
+            }
+            counting ++;
+        }
+    }
+
     @Override
     public void teleopPeriodic() {
-
-
+        moveForward(12, 1);
+        turn(90, 2, true);
+        moveForward(6, 3);
+        turn(90, 4, false);
+        moveForward(24, 5);
+        turn(90, 6, true);
+        moveForward(15, 6);
+        turn(90, 7, false);
+        moveForward(6, 8);
+        turn(80, 9, true);
+        moveForward(24, 10);
+        turn(70, 11, false);
+        moveForward(36, 12);
+        turn(70, 13, false);
     }
     //Runs when antonomous mode (robot runs on its own) first activated via the desktop application
     @Override
     public void autonomousInit() {
-        while (encoder.getDistance() < 12*TICKS_PER_INCH) {
-            TrapezoidProfile.State profileOutput = profile.calculate(0.02 * counter1);
-            controller.setSetpoint(12 * TICKS_PER_INCH);
-            double output = controller.calculate(encoder.getDistance());
-            sparkRight.set(output);
-            sparkLeft.set(output);
-            counter1++;
-        }
-        greenLight.start();
-        while (gyro.getAngleZ() < 90) {
-            sparkLeft.set(0.5);
-            sparkRight.set(-0.5);
-        }
-        greenLight.close();
-        redLight.start();
-        while (encoder.getDistance() < 18*TICKS_PER_INCH) {
-            TrapezoidProfile.State profileOutput = profile.calculate(0.02 * counter2);
-            controller.setSetpoint(6 * TICKS_PER_INCH);
-            double output = controller.calculate(encoder.getDistance());
-            sparkRight.set(output);
-            sparkLeft.set(output);
-            counter2++;
-        }
-        redLight.close();
-        yellowLight.start();
-        while (gyro.getAngleZ() > 0) {
-            sparkLeft.set(-0.5);
-            sparkRight.set(0.5);
-        }
-        yellowLight.close();
-        greenLight.start();
-        while (encoder.getDistance() < 42*TICKS_PER_INCH) {
-            TrapezoidProfile.State profileOutput = profile.calculate(0.02 * counter3);
-            controller.setSetpoint(24 * TICKS_PER_INCH);
-            double output = controller.calculate(encoder.getDistance());
-            sparkRight.set(output);
-            sparkLeft.set(output);
-            counter3++;
-        }
-        greenLight.close();
-        redLight.start();
-        while (gyro.getAngleZ() < 90) {
-            sparkLeft.set(0.5);
-            sparkRight.set(-0.5);
-        }
-        redLight.close();
-        yellowLight.start();
-        while (encoder.getDistance() < 57*TICKS_PER_INCH) {
-            TrapezoidProfile.State profileOutput = profile.calculate(0.02 * counter4);
-            controller.setSetpoint(15 * TICKS_PER_INCH);
-            double output = controller.calculate(encoder.getDistance());
-            sparkRight.set(output);
-            sparkLeft.set(output);
-            counter4++;
-        }
-        yellowLight.close();
-        greenLight.start();
-        while (gyro.getAngleZ() > 0) {
-            sparkLeft.set(-0.5);
-            sparkRight.set(0.5);
-        }
-        greenLight.close();
-        redLight.start();
-        while (encoder.getDistance() < 63*TICKS_PER_INCH) {
-            TrapezoidProfile.State profileOutput = profile.calculate(0.02 * counter5);
-            controller.setSetpoint(6 * TICKS_PER_INCH);
-            double output = controller.calculate(encoder.getDistance());
-            sparkRight.set(output);
-            sparkLeft.set(output);
-            counter5++;
-        }
-        redLight.close();
-        yellowLight.start();
-        while (gyro.getAngleZ() < 80) {
-            sparkLeft.set(0.5);
-            sparkRight.set(-0.5);
-        }
-        yellowLight.close();
-        greenLight.start();
-        while (encoder.getDistance() < 87*TICKS_PER_INCH) {
-            TrapezoidProfile.State profileOutput = profile.calculate(0.02 * counter6);
-            controller.setSetpoint(24 * TICKS_PER_INCH);
-            double output = controller.calculate(encoder.getDistance());
-            sparkRight.set(output);
-            sparkLeft.set(output);
-            counter6++;
-        }
-        greenLight.close();
-        redLight.start();
-        while (gyro.getAngleZ() > 10) {
-            sparkLeft.set(-0.5);
-            sparkRight.set(0.5);
-        }
-        redLight.close();
-        yellowLight.start();
-        while (encoder.getDistance() < 123*TICKS_PER_INCH) {
-            TrapezoidProfile.State profileOutput = profile.calculate(0.02 * counter7);
-            controller.setSetpoint(36 * TICKS_PER_INCH);
-            double output = controller.calculate(encoder.getDistance());
-            sparkRight.set(output);
-            sparkLeft.set(output);
-            counter7++;
-        }
-        yellowLight.close();
-        greenLight.start();
-        while (gyro.getAngleZ() > -60) {
-            sparkLeft.set(-0.5);
-            sparkRight.set(0.5);
-        }
-        greenLight.close();
-        redLight.start();
-        while (encoder.getDistance() < 153*TICKS_PER_INCH) {
-            TrapezoidProfile.State profileOutput = profile.calculate(0.02 * counter8);
-            controller.setSetpoint(36 * TICKS_PER_INCH);
-            double output = controller.calculate(encoder.getDistance());
-            sparkRight.set(output);
-            sparkLeft.set(output);
-            counter8++;
-        }
-        redLight.close();
+
     }
 
     //Runs when teleoperated mode (robot controlled by driver) is first activated
