eca1aa5cb0f772346f999e2b69b431d24e4b2032
emmashim <139157140+emmashim@users.noreply.github.com>
2023-07-11 11:59:47 -0700

    day 2

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -17,13 +17,45 @@ public class Robot extends TimedRobot {
     /*
      *  INITIALIZE CLASSES HERE
      */
-
-
+        //declaring motor objects (left and right motor and encoder + middle encoder)
+        Spark leftMotor;
+        Encoder leftEncoder;
+        Spark rightMotor;
+        Encoder rightEncoder;
+        Encoder encoder;
+        //declaring button objects
+        DigitalInput a_button, b_button, c_button;
+        //declaring gyro
+        RomiGyro gyro;
 
     @Override
     public void robotInit() {
+        //initializing motor objects
+        sparkLeft = new Spark(0);
+        sparkRight = new Spark(1);
 
+        //initializing button objects
+        a_button = new DigitalInput(0);
+        b_button = new DigitalInput(2);
+        c_button = new DigitalInput(1);
 
+        //initialize encoders
+        leftEncoder = new Encoder(Constants.LEFT_ENCODER_IDS[0], Constants.LEFT_ENCODER_IDS[1]);
+        leftEncoder.reset();
+        leftEncoder.setDistancePerPulse(1./10.);
+        leftEncoder.setMinRate(10.);
+        leftEncoder.setReverseDirection(true);
+
+        rightEncoder = new Encoder(Constants.RIGHT_ENCODER_IDS[0], Constants.RIGHT_ENCODER_IDS[1]);
+        rightEncoder.setDistancePerPulse(1./Constants.TICKS_PER_INCH);
+        rightEncoder.reset();
+
+        encoder = new Encoder(Constants.RIGHT_ENCODER_IDS[0], Constants.RIGHT_ENCODER_IDS[1]);
+        encoder.setDistancePerPulse(1.Constants.TICKS_PER_INCH);
+        encoder.reset();
+
+        gyro = new RomiGyro();
+        gyro.reset();
     }
 
     //Runs periodically during teleoperated mode
@@ -32,10 +64,112 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void teleopPeriodic() {
+        /*
+        if(a_button.get() && b_button.get() && c_button.get()){
+            //REVERSE
+            sparkLeft.set(-0.5);
+            sparkRight.set(-0.5);
+        } else if(a_button.get() && b_button.get()){
+            //FORWARD
+            sparkLeft.set(0.5);
+            sparkRight.set(0.5);
+        } else if(b_button.get() && c_button.get()){
+            //TURN LEFT
+            sparkLeft.set(-0.5);
+            sparkRight.set(0.5);
+        } else if(a_button.get() && c_button.get()){
+            //TURN RIGHT
+            sparkLeft.set(0.5);
+            sparkRight.set(-0.5);
+        } else if(a_button.get){
+            //TURN RIGHT THEN DRIVE FORWARD THEN STOP
+            sparkLeft.set(0.5);
+            sparkRight.set(-0,5);
+        } else if(b_button.get){
+            sparkLeft.set(0.5);
+            sparkRight.set(0.5);
+        } else if(c_button.get){
+            sparkLeft.set(0);
+            sparkRight.set(0);
+        }
 
-    }
+        double distance = leftEncoder.getDistance();
+        double rate = leftEncoder.getRate();
+        double angleZ = gyro.getAngleZ();
 
-    //Runs when antonomous mode (robot runs on its own) first activated via the desktop application
+        //drive 48 inches
+        if(encoder.getDistance() >= 48){
+            leftSpark.set(0);
+            rightSpark.set(0);
+        } else {
+            leftSpark.set(0.5);
+            rightSpark.set(0.5);
+        }
+
+        //make 90 degree turn
+        if(angleZ<90)[
+            leftSpark.set(0.25);
+            rightSpark.set(-0.25);
+        ]else if(angleZ>90){
+            leftSpark.set(-0.25);
+            rightSpark.set(-0.25);
+
+        } else(
+            leftSpark.set(0);
+            rightSpark.set(0);
+        )
+    
+    */
+
+   if[encoder.getDistance()>= 101.5][
+    left Spark(0);
+    rightSpark(0);
+    ]
+   else {
+    leftSpark.set(1);
+    rightSpark.set(1);
+   }
+//------------------------
+   if [angleZ<90]{
+    leftSpark.set(0.5);
+    rightSpark.set(-0.5);
+    }else if[angleZ>90]{
+    leftSpark.set(-0.5);
+    rightSpark.set(0.5);
+    encoder.reset();
+    }else{
+        if [encoder.getDistance()<72]{
+            leftSpark.set(1);
+            rightSpark.set(1);
+            }}
+//------------------------
+   } if [angleZ<45]{
+    leftSpark.set(0.5);
+    rightSpark.set(-0.5);
+    }else if[angleZ>45]{
+    leftSpark.set(-0.5);
+    rightSpark.set(0.5);
+    encoder.reset();
+    }else{
+        if [encoder.getDistance()<44]{
+            leftSpark.set(1);
+            rightSpark.set(1);
+            }}
+//------------------------
+   } if [angleZ<135]{
+    leftSpark.set(0.5);
+    rightSpark.set(-0.5);
+    }else if[angleZ>135]{
+    leftSpark.set(-0.5);
+    rightSpark.set(0.5);
+    encoder.reset();
+    }else{
+        if [encoder.getDistance()<]55{
+            leftSpark.set(1);
+            rightSpark.set(1);
+            }}
+
+    //Runs when autonomous mode (robot runs on its own) first activated via the desktop application
     @Override
     public void autonomousInit() {
 
